c1450abba159ccde2d4cec74c2b290cfb10a891e
CIFU1199 <edwkamilo@gmail.com>
2022-05-17 19:24:19 -0500

    agregar js

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+(function(){
+    const sliders = [...document.querySelectorAll('.testimony-body')];
+    const ButtonNext = document.querySelector('#next');
+    const ButtonBefore = document.querySelector('#before');
+    let value;
+
+    ButtonNext.addEventListener('click',()=>{
+        changePosition(1);
+    })
+    ButtonBefore.addEventListener('click',()=>{
+        changePosition(-1);
+    })
+
+    const changePosition = (add) =>{
+        const currentTestimony = document.querySelector('testimony-body--show').dataset.id;
+    }
+
+})();
